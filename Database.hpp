b13775fbaa6c2f6b125abf162730cf1735127841
OlivierOlla <olivier_ollagnier@hotmail.fr>
2024-03-19 15:23:44 +0100

    X

--- Database.hpp
+++ Database.hpp
@@ -5,10 +5,10 @@
 #include <string>
 #include <openssl/evp.h> // Pour le hachage SHA256
 
-// classe pour stocker les utilisateurs et leurs mdp
+// classe pour stocker les utilisateurs et leurs mots de passe
 class Database {
 private:
-    // unordered pour stocker les usernames et leurs mdp
+    // unordered sert à stocker les utilisateurs et leurs mots de passe
     std::unordered_map<std::string, std::string> users;
 
     std::string hashPassword(const std::string& password);
@@ -18,7 +18,7 @@ public:
     bool authenticate(const std::string& username, const std::string& password);
 };
 
-// méthode privée pour hasher le mdp
+// méthode privée pour hasher le mot de passe
 std::string Database::hashPassword(const std::string& password) {
     // prend un mot de passe en clair en entrée et
     // retourne le mot de passe haché à l'aide de l'algorithme de hachage SHA256 fourni par OpenSSL
